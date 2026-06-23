313c36ab3e3c177068b339648f1cbd16ae30f2f0
Manthri kavyashree <kavyashreemanthri@gmail.com>
2026-06-23 20:15:59 +0530

    Update frontend API URL for deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://transaction-ranking-system-7szd.onrender.com";
 
 // Add Transaction
 document.getElementById("transactionForm").addEventListener("submit", async (e) => {
